ceb3199df682c8f414df0695a1d66b3a7e127edf
Hunter Evanoff <hunter547@gmail.com>
2023-03-21 20:21:05 -0600

    Initial commit.

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    description: "Student Doctor Portfolio for Toma Evanoff",
+    locale: "en",
+    showThemeLogo: false,
+    title: "Toma Evanoff Portfolio",
+    formspreeEndpoint: "https://formspree.io/f/{your-id}",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss")(require("./tailwind.config")("rocky-vista")),
+          require("postcss-input-range"),
+          require("autoprefixer"),
+        ],
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: "content/",
+      },
+    },
+    `gatsby-plugin-react-svg`,
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // {
+    //   resolve: 'gatsby-plugin-manifest',
+    //   options: {
+    //     icon: 'content/images/favicon.png',
+    //   },
+    // },
+  ],
+};
---
+++ src/themes/rocky-vista.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'back': '#f0f0f0',
+  'front': '#24437c',
+  'lead': '#fba31c',
+  'lead-text': '#ffffff',
+  'line': '#dedede',
+  'skill-1': '#1e2127',
+  'skill-2': '#393e46',
+  'skill-3': '#5c636e',
+};
